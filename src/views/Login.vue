b693ecdb484c0b8dc176ec75be027bd56fd03c3f
ganlinlaomu <101681744+ganlinlaomu@users.noreply.github.com>
2025-12-17 12:14:48 +0900

    Fix HTML tags in Login.vue template

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="card login-card">
     <div class="login-center">
-      <h1 class="title">知己>zhi 
+      <h1 class="title">知己</h1>
 
       <div class="login-info">
-        <p>真正的朋友圈<p>
+        <p>真正的朋友圈</p>
       </div>
 
       <!-- Login Actions -->
