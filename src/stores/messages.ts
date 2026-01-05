e1fa771a0b245b1f5bd8cb9c3d1c8a12726027c1
ganlinlaomu <101681744+ganlinlaomu@users.noreply.github.com>
2026-01-06 08:54:14 +0900

    更新 messages.ts

--- src/stores/messages.ts
+++ src/stores/messages.ts
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import { useKeyStore } from "./keys";
-import { getDatabase } from "@/db/dexie";
+import { getCurrentDatabase } from "@/db/dexie";
 
 /* =========================
  * Types
